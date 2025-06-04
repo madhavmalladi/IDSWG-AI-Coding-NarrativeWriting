511b0ad584bc40510627c464a2cb94ed42c10574
madhavmalladi <madhav.malladi25@gmail.com>
2025-06-03 19:49:17 -0700

    added llama summarizer button

--- narrative_writer.py
+++ narrative_writer.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from striprtf.striprtf import rtf_to_text
+import requests
 import pandas as pd
 import numpy as np
 
@@ -10,3 +11,11 @@ if uploaded_file:
     content = uploaded_file.getvalue().decode("utf-8")
     # Convert the rtf file into normal text
     text = rtf_to_text(content)
+    input = st.text_area("Input to model:", value = text, height = 300)
+
+    # Running the model
+    if st.button("Run Llama Summary"):
+        with st.spinner("Summarizing"):
+            response = requests.post("http://localhost:11434/api/generate", json = {"model": "llama2", "prompt": input, "stream": False})
+            result = response.json()
+            st.text_area("Llama output:", result, height = 300)
