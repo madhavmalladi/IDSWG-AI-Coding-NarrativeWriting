df49b7926dde7368b6a86c2defdb97a6e91c0b3c
madhavmalladi <madhav.malladi25@gmail.com>
2025-06-03 19:36:46 -0700

    initialized streamlit app

--- narrative_writer.py
+++ narrative_writer.py
@@ -1,5 +1,12 @@
 import streamlit as st
+from striprtf.striprtf import rtf_to_text
 import pandas as pd
 import numpy as np
 
 st.title("AI Patient Summarizer")
+
+uploaded_file = st.file_uploader("Please Upload the Patient Data (RTF)", type = "rtf")
+if uploaded_file:
+    content = uploaded_file.getvalue().decode("utf-8")
+    # Convert the rtf file into normal text
+    text = rtf_to_text(content)
